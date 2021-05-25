251a4b74b5568242ef68b16a08b4d2870c3da9fc
KeA <734682120@qq.com>
2021-05-25 10:01:44 +0800

    创建项目

---
+++ src/main/java/red/kea/keabit/KeabitApplication.java
@@ -0,0 +1,13 @@
+package red.kea.keabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeabitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeabitApplication.class, args);
+    }
+
+}
